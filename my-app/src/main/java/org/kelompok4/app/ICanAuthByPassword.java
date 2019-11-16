85d5d0369db22a34364ca5ae9541eca032bc83f3
Fakhri Aunurrahim <fakhri.aunur@gmail.com>
2019-11-16 14:27:54 +0700

    MVC Login redo1

---
+++ my-app/src/main/java/org/kelompok4/app/ICanAuthByPassword.java
@@ -0,0 +1,7 @@
+package org.kelompok4.app;
+
+public interface ICanAuthByPassword {
+
+    public abstract String getLoginPassword();
+    public abstract void setLoginPassword(String login);
+}
---
+++ my-app/src/main/java/org/kelompok4/app/LoginView.java
@@ -0,0 +1,23 @@
+package org.kelompok4.app;
+public class LoginView {
+
+	public LoginView(){
+
+	}
+	public void printLoginPage() {
+		System.out.println("#LOGIN SISTEM#");
+		System.out.println("");
+		System.out.println("");
+
+	}
+
+	public void printEmailPrompt() {
+		System.out.print("Email : ");
+		System.out.flush();
+	}
+
+	public void printPasswordPrompt() {
+		System.out.print("Password : ");
+		System.out.flush();
+	}
+}

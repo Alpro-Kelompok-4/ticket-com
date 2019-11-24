eb53acaa67d9cccb7e1a2491d8652f0eb9678094
arfan <marfanw@gmail.com>
2019-11-24 11:13:07 +0700

    add class

---
+++ src/main/java/org/kelompok4/app/Controller/RouteTrainController.java
@@ -0,0 +1,24 @@
+package org.kelompok4.app.Controller;
+
+import org.kelompok4.app.Interface.ICanCreate;
+import org.kelompok4.app.Interface.ICanDelete;
+import org.kelompok4.app.Interface.ICanRead;
+
+public class RouteTrainController implements ICanCreate, ICanRead, ICanDelete {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+}
---
+++ src/main/java/org/kelompok4/app/Controller/TrainScheduleController.java
@@ -0,0 +1,18 @@
+package org.kelompok4.app.Controller;
+
+import org.kelompok4.app.Interface.ICanCreate;
+import org.kelompok4.app.Interface.ICanRead;
+
+public class TrainScheduleController implements ICanCreate, ICanRead {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+}
